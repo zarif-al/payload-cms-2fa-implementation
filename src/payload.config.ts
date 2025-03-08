eee8dbc665a96c7d205f11b841560ed57c67a164
Abdullah Al Zarif <zarif_al96@outlook.com>
2025-03-08 16:43:49 +0600

    feat: replace en translations, update default login route, add custom login component to `/login` route

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -10,15 +10,35 @@ import sharp from 'sharp'
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
 import { OTP } from './collections/OTP'
+import { enTranslations } from './app/(payload)/translations/en'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+  i18n: {
+    translations: {
+      en: enTranslations,
+    },
+  },
   admin: {
     user: Users.slug,
     importMap: {
       baseDir: path.resolve(dirname),
+    },
+    // Moving default login page to deprecated route.
+    routes: {
+      login: '/deprecated-login',
+    },
+    components: {
+      views: {
+        customLogin: {
+          Component: {
+            path: '/app/(payload)/components/login#LoginView',
+          },
+          path: '/login',
+        },
+      },
     },
   },
   collections: [Users, Media, OTP],
