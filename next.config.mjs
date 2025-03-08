b1044cb27b981696510002a1bbc6156300bcb5fb
Abdullah Al Zarif <zarif_al96@outlook.com>
2025-03-08 16:44:32 +0600

    feat: permanent redirect from payload default login route to the custom login view's login route

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  redirects: async () => {
+    return [
+      // Redirect from payloads default login to our custom login path
+      {
+        source: '/admin/deprecated-login',
+        destination: '/admin/login',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default withPayload(nextConfig)
