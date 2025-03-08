3513d80c174348e6c838b96275dbfe1da8484488
Abdullah Al Zarif <zarif_al96@outlook.com>
2025-03-08 16:43:59 +0600

    build: update import map

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,1 +1,5 @@
-export const importMap = {}
+import { LoginView as LoginView_21b720baacbe0d6ce78a70840f96ccc4 } from 'src/app/(payload)/components/login'
+
+export const importMap = {
+  "/app/(payload)/components/login#LoginView": LoginView_21b720baacbe0d6ce78a70840f96ccc4
+}
